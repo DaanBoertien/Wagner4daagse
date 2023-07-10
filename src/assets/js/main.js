4ec47875ef25fa4fec854217831f11d234b3e0e0
daanb <daan.boertien@gmail.com>
2023-07-10 17:06:32 +0200

    started with menu

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,43 @@
+document.querySelector('.hamburger').addEventListener('click', function(e) {
+  e.preventDefault();
+  if(!this.classList.contains('is-open')) {
+      this.classList.add('is-open');
+  } else {
+      this.classList.remove('is-open');
+  }
+});
+
+document.querySelector('.hamburger').addEventListener('click', function() {
+  this.classList.toggle('is-open');
+  document.querySelector('nav').classList.toggle('is-open');
+});
+
+document.querySelector(".dropdown-toggle").addEventListener('click', function (event) {
+  event.stopPropagation();
+  document.getElementById("dropdownMenu").classList.toggle("show");
+});
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropdown-toggle')) {
+      var dropdowns = document.getElementsByClassName("dropdown-menu");
+      for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
+      }
+  }
+}
+
+
+document.querySelector('.dropdown-toggle').addEventListener('click', function(event) {
+  this.parentNode.classList.toggle('is-open');
+});
+
+
+
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
@@ -10,4 +50,4 @@ const swiper = new Swiper('.swiper', {
     },
   });
 
-  console.log("hoi");
+  
