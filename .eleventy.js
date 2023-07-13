96ace838a8e01efd03134fd885c9225579ac1569
daanb <daan.boertien@gmail.com>
2023-07-13 13:27:13 +0200

    added lots of functionality

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,26 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/css");
-    eleventyConfig.addPassthroughCopy("src/assets");
-    eleventyConfig.addPassthroughCopy("src/_includes/partials");
+const markdownIt = require('markdown-it');
+const yaml = require("js-yaml");
 
-      
-  
-    return {
-      dir: {
-        input: "src",
-        output: "_site"
-      }
-    };
+module.exports = function(eleventyConfig) {
+  let markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true
+  });
+
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/_includes/partials");
+  eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
+  eleventyConfig.setLibrary("md", markdownLibrary);
+  eleventyConfig.addFilter('markdownify', (markdownString) => {
+    return markdownLibrary.render(markdownString);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
   };
-  
+};
